6036d6d66a909e3a2c9e3491ec42d5499dbe8fc3
kjkandrea <kjkandrea@gmail.com>
2023-02-12 10:43:14 +0900

    feat: status 출력

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 export function main() {
   fetch('https://picsum.photos/200/300').then(response => {
+    console.group(response.url);
+    console.log('status :', response.status);
     console.log('content-type :', response.headers.get('content-type'));
     console.log('content-length :', response.headers.get('content-length'));
+    console.groupEnd();
   });
 }
 
